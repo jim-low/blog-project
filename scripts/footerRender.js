3ff796fce9b526c2ef26264a2e3560e3f533da13
jim-low <robil12380@gmail.com>
2020-11-01 11:55:56 +0800

    finished functionality for footer rendering

---
+++ scripts/footerRender.js
@@ -0,0 +1,41 @@
+document.querySelector("footer").innerHTML +=
+		`<div class="info">
+			<div class="col contact">
+				<h3>contact us</h3>
+				<div class="person jim">
+					<h4>jim low lap hong</h4>
+					<div class="social-media">
+						<a href="#" class="link"><i class="far fa-envelope"></i></a>
+						<a href="#" class="link"><i class="fab fa-linkedin-in"></i></a>
+						<a href="#" class="link"><i class="fab fa-instagram"></i></a>
+						<a href="#" class="link"><i class="fab fa-github"></i></a>
+					</div>
+				</div>
+				<div class="person jason">
+					<h4>jason liew jun xian</h4>
+					<div class="social-media">
+						<a href="#" class="link"><i class="far fa-envelope"></i></a>
+						<a href="#" class="link"><i class="fab fa-linkedin-in"></i></a>
+						<a href="#" class="link"><i class="fab fa-instagram"></i></a>
+						<a href="#" class="link"><i class="fab fa-github"></i></a>
+					</div>
+				</div>
+			</div>
+			<div class="col product">
+				<h3>blog</h3>
+				<a href="#" class="link">products</a>
+				<a href="#" class="link">services</a>
+				<a href="#" class="link">private policty</a>
+				<a href="#" class="link">terms and conditions</a>
+			</div>
+			<div class="col partnership">
+				<h3>partnership</h3>
+				<a href="#" class="link">criteria</a>
+				<a href="#" class="link">join the team!</a>
+			</div>
+		</div>
+		<div class="cpy-right">
+			<p class="cpy-right-para">
+				Copyright &copy; | Created and maintained by <span class="person">jim low lap hong</span> and<spanclass="person">jason liew jun xian</spanclass=>
+			</p
+		</div>`
