87794fabdd0142908d404753ca38295d33afaef8
jim-low <robil12380@gmail.com>
2021-01-21 16:27:49 +0800

    Added file to store projects and file to execute rendering

---
+++ renderData.js
@@ -0,0 +1,136 @@
+const imgDir = './images/'
+
+const credentials = [
+    {
+        owner: "jim low lap hong",
+        status: ["currently studying in Diploma"],
+        proficiency: ['C Programming', 'C Programming', 'C Programming'],
+        experience: ['to be continued', 'to be continued', 'to be continued'],
+        onlinePresence: ['LinkedIn', 'Github', 'Email Account'],
+    },
+    {
+        owner: "jason liew jun xian",
+        status: ["currently studying in Diploma"],
+        proficiency: ['C Programming', 'C Programming', 'C Programming'],
+        experience: ['to be continued', 'to be continued', 'to be continued'],
+        onlinePresence: ['LinkedIn', 'Github', 'Email Account'],
+    },
+]
+
+const projectsList = [
+    {
+        owner: "jim low lap hong",
+        projects: 
+        [
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+        ]
+    },
+    {
+        owner: "jason liew jun xian",
+        projects: 
+        [
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+        ]
+    },
+    {
+        owner: "team projects",
+        projects: 
+        [
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+            {
+                title: 'foo',
+                image: 'img.png',
+                link: 'wherever.com',
+                desc: 'great project'
+            },
+        ]
+    }
+]
+
+export {
+    imgDir,
+    projectsList,
+    credentials,
+}
+
